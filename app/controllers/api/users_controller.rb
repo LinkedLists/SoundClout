8e2b291cc4ec004ec78cacbc640526ad5162f3d4
appkenny <kzeng.95@gmail.com>
2021-02-10 01:41:41 -0800

    added a user show action and debugged code on the server

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,6 +9,13 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user
+      render 'api/users/show'
+    end
+  end
+
 
   private
   def user_params
