51af0d8326425a2ac11ea734380493752629f20f
appkenny <kzeng.95@gmail.com>
2021-02-11 10:29:19 -0800

    dropped use of email from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@
 #
 #  id              :bigint           not null, primary key
 #  username        :string           not null
-#  email           :string           not null
 #  password_digest :string           not null
 #  session_token   :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-  validates :username, :email, :session_token, :password_digest, presence: true
-  validates :username, :email, uniqueness: true
+  validates :username, :session_token, :password_digest, presence: true
+  validates :username, uniqueness: true
   validates :password, length: {minimum: 6}, allow_nil: true
   after_initialize :ensure_session_token
   attr_reader :password
