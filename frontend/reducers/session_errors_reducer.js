1eb8c9028f3105ce442268b6923322bfeb895e9e
appkenny <kzeng.95@gmail.com>
2021-02-10 18:03:43 -0800

    created entities and errors reducer

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS } from '../actions/session_actions';
+
+// ActiveRecord will be storing validation errors in an array
+const sessionErrorsReducer = (oldState = [], action) => {
+  Object.freeze(oldState);
+  newState = Object.assign([]. oldState);
+
+  switch(action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors
+    case RECEIVE_CURRENT_USER:
+      return []
+    default:
+      return oldState
+  }
+}
+
+export default sessionErrorsReducer
