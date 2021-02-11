35b4ec41d49ed5e7250af14bf386516c1e39578e
appkenny <kzeng.95@gmail.com>
2021-02-11 11:59:13 -0800

    created status errors and can now render errors

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,28 +1,21 @@
 class Api::SessionsController < ApplicationController
 
-  # def new
-  #   @user = User.new
-  #   render 
-  # end
-
   def create
     @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
     if @user 
       login(@user)
       render '/api/users/show'
     else
-      render @user.error.full_messages
+      render json: ["Invalid credentials. Please try again!"], status 401
     end
   end
 
   def destroy
     if current_user
       logout
-      render :text => "logged out"
+      render json: ["You have logged out"]
     else
-      render :text => "not signed in"
+      render json: ["No user signed in"], status: 404
     end
-
   end
-
 end
