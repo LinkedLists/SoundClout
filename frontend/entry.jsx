95298a1efbadaf08cf5be7c65f821f326b9e2b31
appkenny <kzeng.95@gmail.com>
2021-02-11 01:18:53 -0800

    created app and root files in component as well as creating a signup form

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
-// import configureStore from './store/store'
-// import Root from './components/root';
+import Root from './components/root';
 
-const store = configureStore();
-
-window.getState = store.getState;
-window.dispatch = store.dispatch;
 document.addEventListener('DOMContentLoaded', () => {
-  // const root = document.getElementById('root');
-  // ReactDOM.render(<h1>hello</h1>, root);
+  const store = configureStore();
+  
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
 })
