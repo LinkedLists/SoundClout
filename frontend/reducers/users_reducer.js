b75a18eb592b25f652560db8d97a4e43779e0340
appkenny <kzeng.95@gmail.com>
2021-02-10 17:35:30 -0800

    cleaned up session reducer and added a users reducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const userReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  newState = Object.assign({}, oldState);
+
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return newState[action.user.id] = action.user 
+    default:
+      return oldState
+  }
+}
+
+export default userReducer
