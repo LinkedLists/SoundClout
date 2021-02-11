c456f3322adb15832d98c13de6058f8c35b52d34
appkenny <kzeng.95@gmail.com>
2021-02-11 01:56:20 -0800

    added jquery in application.js and made minor changes to session action and reducer to remove bugs

--- frontend/components/session_form/signup_form.jsx
+++ frontend/components/session_form/signup_form.jsx
@@ -17,6 +17,11 @@ class SignUpForm extends React.Component {
     e.preventDefault();
     const user = Object.assign({}, this.state);
     this.props.signup(user);
+    this.setState({
+      email: '',
+      password: ''
+    })
+    console.log("i am here")
   }
 
   handleChange(field) {
@@ -29,11 +34,11 @@ class SignUpForm extends React.Component {
         <h1>hello</h1>
         <form onSubmit={this.handleSubmit}>
           <label>Email:
-            <input type='text' onChange={this.handleChange('email')} value={this.state.email}></input>
+            <input type='text' onChange={this.handleChange('email')} value={this.state.email} />
           </label>
           <br/>
           <label>Password:
-            <input type='text' onChange={this.handleChange('password')} value={this.state.password}></input>
+            <input type='text' onChange={this.handleChange('password')} value={this.state.password} />
           </label>
           <br/>
           <input type='submit' value="Sign Up" />
