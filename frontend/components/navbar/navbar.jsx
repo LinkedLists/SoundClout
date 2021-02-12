cf932c42d9321683b9235c2c328fead58650e21a
appkenny <kzeng.95@gmail.com>
2021-02-12 02:27:58 -0800

    made changes to navbar to make the modal appear

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { openModal } from '../../actions/modal_actions';
 
 class NavBar extends React.Component {
   render() {
     return (
       <div>
         <h1>this is the navbar</h1>
-        <Link to='/signup'>Sign Up</Link>
-        <Link to='/login'>Login</Link>
+
+
+        {/* taken from a/A */}
+        <nav className="login-signup">
+          <button onClick={() => openModal('login')}>Login</button>
+          &nbsp;or&nbsp;
+          <button onClick={() => openModal('signup')}>Signup</button>
+        </nav>
       </div>
     )
   }
