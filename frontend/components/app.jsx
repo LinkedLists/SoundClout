3ae0f0dc60974a21d0d4b74e5b0dfafcdaa9a3b5
appkenny <kzeng.95@gmail.com>
2021-02-12 15:45:22 -0800

    added comments to routes in app.jsx and made first css commit

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,11 +9,18 @@ import NotFound from './404/not_found'
 // Note that session containers will no longer be necessary due to use of a modal
 const App = () => (
   <div>
-    <Modal />
+
+    {/* Route bugs: 
+    1. rendering NotFound component does not clear entire page. If modal is open
+    it will remain open when user routes to a undef. route
+    2. after showing the NotFound component if user goes back one page it will display 
+    an error and will need to refresh the page for components to rerender
+    */}
     <Switch>
       <Route exact path='/' component={NavBarContainer} />
       <Route component={NotFound} />
     </Switch>
+    <Modal />
 
     {/* <Route path='/signup' component={SignupFormContainer} />
     <Route path='/login' component={LoginFormContainer} /> */}
