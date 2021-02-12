6a435bba60c0c6f564836c26ecb81024d6a98f18
appkenny <kzeng.95@gmail.com>
2021-02-12 15:11:17 -0800

    added a 404 error page and display proper session option after login

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 // import SignupFormContainer from './session_form/signup_container'
 // import LoginFormContainer from './session_form/login_container'
 import NavBarContainer from './navbar/navbar_container'
 import Modal from './modal/modal';
+import NotFound from './404/not_found'
 
 // Note that session containers will no longer be necessary due to use of a modal
 const App = () => (
   <div>
-    <h1>This is the app</h1>
     <Modal />
-    <Route path='/' component={NavBarContainer} />
+    <Switch>
+      <Route exact path='/' component={NavBarContainer} />
+      <Route component={NotFound} />
+    </Switch>
 
     {/* <Route path='/signup' component={SignupFormContainer} />
     <Route path='/login' component={LoginFormContainer} /> */}
--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 class NavBar extends React.Component {
+
+  
   render() {
     return (
       <div>
         <h1>this is the navbar</h1>
-
-
         {/* taken from a/A */}
-        <nav className="login-signup">
-          <button onClick={() => this.props.openModal('login')}>Login</button>
-          &nbsp;or&nbsp;
-          <button onClick={() => this.props.openModal('signup')}>Signup</button>
-        </nav>
-
-        {this.props.state.session ? 
-        <button onClick={() => this.props.logout()}>Logout</button> : null 
+        {
+          this.props.state.session.id ? 
+          <button onClick={() => this.props.logout()}>Logout for user #{this.props.state.session.id}</button> : 
+          <nav className="login-signup">
+            <button onClick={() => this.props.openModal('login')}>Login</button>
+            &nbsp;or&nbsp;
+            <button onClick={() => this.props.openModal('signup')}>Signup</button>
+          </nav>
         }
 
       </div>
