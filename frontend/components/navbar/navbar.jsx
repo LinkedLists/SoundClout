d6ec513770bc02735e865ea36091a644a02abfb8
appkenny <kzeng.95@gmail.com>
2021-02-12 17:55:58 -0800

    added some basic css to the homepage and modal

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -7,15 +7,15 @@ class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <h1>this is the navbar</h1>
+
         {/* taken from a/A */}
         {
           this.props.state.session.id ? 
           <button onClick={() => this.props.logout()}>Logout for user #{this.props.state.session.id}</button> : 
           <nav className="login-signup">
-            <button onClick={() => this.props.openModal('login')}>Login</button>
-            &nbsp;or&nbsp;
-            <button onClick={() => this.props.openModal('signup')}>Signup</button>
+            <button className='signup' onClick={() => this.props.openModal('signup')}>Create Account</button>
+
+            <button className='login' onClick={() => this.props.openModal('login')}>Login</button>
           </nav>
         }
 
