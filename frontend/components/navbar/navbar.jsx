458391856e313fbc5abfb013f9f39754d80bddfe
appkenny <kzeng.95@gmail.com>
2021-02-15 12:07:20 -0800

    added more css and created a placeholder for the logo

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -6,14 +6,18 @@ class NavBar extends React.Component {
   
   render() {
     return (
-      <div>
+      <div className="nav-header-container">
         {
           this.props.state.session.id ? 
           <button onClick={() => this.props.logout()}>Logout for user #{this.props.state.session.id}</button> : 
-          <nav className="login-signup">
-            <button className='signup' onClick={() => this.props.openModal('signup')}>Create Account</button>
+          <nav className="nav-header">
+            <div className="hi">logo placeholder</div>
 
-            <button className='login' onClick={() => this.props.openModal('login')}>Login</button>
+            <div className="login-signup">
+              {/* temp inline style for login */}
+              <button className='login' onClick={() => this.props.openModal('login')} style={{color: 'green'}}>Login</button>
+              <button className='signup' onClick={() => this.props.openModal('signup')}>Create Account</button>
+            </div>
           </nav>
         }
       </div>
