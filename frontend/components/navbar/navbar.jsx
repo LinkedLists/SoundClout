c40b9d287f12e053a461e0143c08b1be97f78cfe
appkenny <kzeng.95@gmail.com>
2021-02-16 00:56:51 -0800

    refactored navbar to conditionally render navbar or session bar

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,24 +3,37 @@ import { Link } from 'react-router-dom'
 
 class NavBar extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.sessionContainer = this.sessionContainer.bind(this);
+    this.navContainer = this.navContainer.bind(this);
+  }
+
+  sessionContainer() {
+    return (
+      <div className="login-signup-container">
+        <nav className="login-signup-nav">
+          <div className="site-logo">logo placeholder</div>
+          <div className="login-signup">
+            {/* temp inline style for login */}
+            <button className='login' onClick={() => this.props.openModal('login')} style={{color: 'green'}}>Login</button>
+            <button className='signup' onClick={() => this.props.openModal('signup')}>Create Account</button>
+          </div>
+        </nav>
+      </div>
+
+)
+}
+navContainer() {
+  return(
+    <button onClick={() => this.props.logout()}>Logout for user #{this.props.state.session.id}</button>
+    )
+  }
   
   render() {
     return (
-      <div className="nav-header-container">
-        {
-          this.props.state.session.id ? 
-          <button onClick={() => this.props.logout()}>Logout for user #{this.props.state.session.id}</button> : 
-          <nav className="nav-header">
-            <div className="hi">logo placeholder</div>
-
-            <div className="login-signup">
-              {/* temp inline style for login */}
-              <button className='login' onClick={() => this.props.openModal('login')} style={{color: 'green'}}>Login</button>
-              <button className='signup' onClick={() => this.props.openModal('signup')}>Create Account</button>
-            </div>
-          </nav>
-        }
-      </div>
+      this.props.state.session.id ? this.navContainer() : this.sessionContainer()
     )
   }
 }
