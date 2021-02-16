f79de678aacf2273c02a1bbe33dd5acb2a86c734
appkenny <kzeng.95@gmail.com>
2021-02-16 12:21:13 -0800

    debugged tracks backend on postman

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@
 #                      root GET    /                                                                                        static_pages#root
 #                 api_users POST   /api/users(.:format)                                                                     api/users#create {:format=>:json}
 #                  api_user GET    /api/users/:id(.:format)                                                                 api/users#show {:format=>:json}
-#           new_api_session GET    /api/session/new(.:format)                                                               api/sessions#new {:format=>:json}
 #               api_session DELETE /api/session(.:format)                                                                   api/sessions#destroy {:format=>:json}
 #                           POST   /api/session(.:format)                                                                   api/sessions#create {:format=>:json}
+#                api_tracks GET    /api/tracks(.:format)                                                                    api/tracks#index {:format=>:json}
+#                           POST   /api/tracks(.:format)                                                                    api/tracks#create {:format=>:json}
+#                 api_track GET    /api/tracks/:id(.:format)                                                                api/tracks#show {:format=>:json}
+#                           DELETE /api/tracks/:id(.:format)                                                                api/tracks#destroy {:format=>:json}
 #        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 # rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
