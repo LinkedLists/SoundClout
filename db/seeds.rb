31ae672abfa5ac581c6a85cd960ddc7235d6aa05
appkenny <kzeng.95@gmail.com>
2021-02-17 12:09:28 -0800

    debugged and issue with rails console and fixed the seed file to attach a track to the demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 User.destroy_all
+Track.destroy_all
+
+
 demo = User.create!(username: "Demo", password: "secretpasswordlol")
 # user1.save!
+# track1 = Track.create!(title: "hello", uploader_id: demo.id, description: "hhahaha", genre: "duh")
+track1 = Track.new(title: "hello", uploader_id: demo.id, description: "hhahaha", genre: "duh")
+# attach image using open uri
+# https://fsp-seed.s3-us-west-1.amazonaws.com/test.jpg
+file = open('https://fsp-seed.s3-us-west-1.amazonaws.com/test.jpg')
+track1.photo.attach(io: file, filename: 'testing')
+track1.save!
+# track1 = Track.create!(title: "hello", uploader_id: 28, description: "hhahaha", genre: "duh")
