4c4341196a7be8c64d0071f1cd80388d37301dd9
appkenny <kzeng.95@gmail.com>
2021-02-18 09:45:34 -0800

    created a route util for auth and protected routes, also dropping 404 component

--- frontend/components/404/not_found.jsx
+++ frontend/components/404/not_found.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default function() {
-  return (
-    <h1 className="error">Turn back. No pokemon can be found on this route!!!</h1>
-  )
+  // 404 component is being scrapped
+  // right now this is being repurposed as an empty component for the auth route
+  return <></>
+    // <h1 className="error">Turn back. No pokemon can be found on this route!!!</h1>
+  
 }
