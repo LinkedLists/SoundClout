202909143e901028e9c43eb4ed60500aab24af5c
appkenny <kzeng.95@gmail.com>
2021-02-18 14:31:01 -0800

    created a simple track show container but now need to fix track slice of state

--- frontend/components/content/content_index_item.jsx
+++ frontend/components/content/content_index_item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 class ContentIndexItem extends React.Component {
   constructor(props) {
@@ -8,10 +9,14 @@ class ContentIndexItem extends React.Component {
   render() {
     return (
       <li className="content-list-item">
-        <img className="content-list-item-img" src={this.props.track.photoUrl}/>
-        <div className="content-list-item-description">
-          {this.props.track.genre}
-        </div>
+        {/* for now the entire item is nested in to the track show */}
+        {/* eventually allow for a link to see the user would be a good idea */}
+        <Link to={`/tracks/${this.props.track.id}`}>
+          <img className="content-list-item-img" src={this.props.track.photoUrl}/>
+          <div className="content-list-item-description">
+            {this.props.track.genre}
+          </div>
+        </Link>
       </li>
     )
   }
