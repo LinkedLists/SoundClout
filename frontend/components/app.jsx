520bd6aef09ec84f22752a8ac125d0f97fb19d14
appkenny <kzeng.95@gmail.com>
2021-02-19 04:58:33 -0800

    added a homepage image

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,6 +5,7 @@ import Modal from './modal/modal';
 import NotFound from './404/not_found'
 import ContentContainer from './content/content_container'
 import TrackShowContainer from './track_show/track_show_container'
+import Homepage from './homepage/homepage'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 // Note that session containers will no longer be necessary due to use of a modal
@@ -21,6 +22,7 @@ const App = () => (
     */}
     <Switch>
       {/* <Route exact path='/' component={NavBarContainer} /> */}
+      {/* <ProtectedRoute exact path='/' component={Homepage} /> */}
       <ProtectedRoute exact path='/discover' component={ContentContainer} />
       <ProtectedRoute exact path='/tracks/:trackId' component={TrackShowContainer} />
   
@@ -28,7 +30,7 @@ const App = () => (
       did not create a homepage component so NotFound component is just an empty component from
       404 since a 404 page is not necessary
       */}
-      <AuthRoute to='/' component={NotFound}/>
+      <AuthRoute exact path='/' component={Homepage}/>
       {/* <Route component={NotFound} /> */}
     </Switch>
   </div>
---
+++ frontend/components/homepage/homepage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class Homepage extends React.Component {
+  render() {
+    return (
+      <div className='homepage-container'>
+        {/* <img src="" */}
+      </div>
+    )
+  }
+}
+
+
+const mapStateToProps = state => {
+  return state
+};
+
+
+export default connect(mapStateToProps, null)(Homepage)
