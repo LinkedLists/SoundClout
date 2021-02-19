fd29a01c764ccb2f8aa84013c00a52b3319ff2fe
appkenny <kzeng.95@gmail.com>
2021-02-19 11:02:33 -0800

    correctly added seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,31 +15,57 @@ demo = User.create!(username: "Demo", password: "secretpasswordlol")
 # user1.save!
 # track1 = Track.create!(title: "hello", uploader_id: demo.id, description: "hhahaha", genre: "duh")
 track1 = Track.new(title: "Saucy", uploader_id: demo.id, description: "hhahaha", genre: "Rock")
-track2 = Track.new(title: "Havana", uploader_id: demo.id, description: "dumbdubmdubmdubm", genre: "Pop")
-track3 = Track.new(title: "Castles Made of Sand", uploader_id: demo.id, description: "vavava", genre: "Rock")
+track2 = Track.new(title: "G.O.A.T", uploader_id: demo.id, description: "timi hendrix", genre: "Rock")
+track3 = Track.new(title: "Havana", uploader_id: demo.id, description: "my new single", genre: "Pop")
+track4 = Track.new(title: "Senorita", uploader_id: demo.id, description: "check out my collab with shawn mende!!", genre: "Pop")
+track5 = Track.new(title: "Castles Made of Sand", uploader_id: demo.id, description: "check out this rare studio version!!", genre: "Rock")
+track6 = Track.new(title: "The Wind Cries Mary", uploader_id: demo.id, description: "another rare find", genre: "Rock")
+track7 = Track.new(title: "Positions", uploader_id: demo.id, description: "here is another bop", genre: "Pop")
+
 # attach image using open uri
 # https://fsp-seed.s3-us-west-1.amazonaws.com/test.jpg
 # https://fsp-seed.s3-us-west-1.amazonaws.com/cc.jpg
 # https://fsp-seed.s3-us-west-1.amazonaws.com/jimi.jpg
 
-file1 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/nlnd.jpg')
-file2 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/cc.jpg')
-file3 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/jimi.jpg')
+image1 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/nlnd.jpg')
+image11 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/nlnd.jpg')
+image2 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/cc.jpg')
+image3 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/senorita.jpg')
+image4 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/jimi.jpg')
+image44 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/jimi.jpg')
+image5 = open('https://fsp-seed.s3-us-west-1.amazonaws.com/positions.png')
 
-audio_2 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Camila+Cabello++Havana+Official+Audio+ft+Young+Thug.mp3")
-audio_3 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Castles+Made+of+Sand+1967.mp3")
-audio_5 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Polyphia++Saucy.mp3")
+audio1 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Polyphia++Saucy.mp3")
+audio2 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Polyphia++GOAT+Official+Music+Video.mp3")
+audio3 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Camila+Cabello++Havana+Official+Audio+ft+Young+Thug.mp3")
+audio4 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Shawn+Mendes+Camila+Cabello++Se%C3%B1orita+Lyrics.mp3")
+audio5 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Castles+Made+of+Sand+1967.mp3")
+audio6 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Jimi+Hendrix+The+Wind+Cries+Mary.mp3")
+audio7 = open("https://fsp-seed.s3-us-west-1.amazonaws.com/yt1s.com+-+Ariana+Grande++positions+Lyrics.mp3")
 
-track1.photo.attach(io: file1, filename: 'testing')
-track2.photo.attach(io: file2, filename: 'havana')
-track3.photo.attach(io: file3, filename: 'jimi')
 
-track1.audio.attach(io: audio_5, filename: 'saucy_audio')
-track2.audio.attach(io: audio_2, filename: 'havana_audio')
-track3.audio.attach(io: audio_3, filename: 'castles_made_of_sand_audio')
+
+track1.photo.attach(io: image1, filename: 'nlnd')
+track2.photo.attach(io: image11, filename: 'nlnd')
+track3.photo.attach(io: image2, filename: 'camila')
+track4.photo.attach(io: image3, filename: 'cc and sm')
+track5.photo.attach(io: image44, filename: 'axis')
+track6.photo.attach(io: image4, filename: 'axis')
+track7.photo.attach(io: image5, filename: 'ariana')
+
+track1.audio.attach(io: audio1, filename: 'saucy_audio')
+track2.audio.attach(io: audio2, filename: 'goat_audio')
+track3.audio.attach(io: audio3, filename: 'havana_audio')
+track4.audio.attach(io: audio4, filename: 'senorita')
+track5.audio.attach(io: audio5, filename: 'castles_made_of_sand_audio')
+track6.audio.attach(io: audio6, filename: 'the_wind_cries_mary_audio')
+track7.audio.attach(io: audio7, filename: 'positions_audio')
 
 
 track1.save!
 track2.save!
 track3.save!
-# track1 = Track.create!(title: "hello", uploader_id: 28, description: "hhahaha", genre: "duh")
+track4.save!
+track5.save!
+track6.save!
+track7.save!
