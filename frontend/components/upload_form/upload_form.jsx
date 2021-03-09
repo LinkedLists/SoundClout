9e515a2c269f2ef403784750a25f5a047be8a31b
appkenny <kzeng.95@gmail.com>
2021-03-09 10:08:14 -0800

    can read files trying to be uploaded

--- frontend/components/upload_form/upload_form.jsx
+++ frontend/components/upload_form/upload_form.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-export default class UploadForm extends React.Component {
+class UploadForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      file: "",
+    }
+
+    this.handleFile = this.handleFile.bind(this);
   }
 
+  handleFile(e) {
+    this.setState({file: e.target.files[0]})
+    console.log(this.state.file)
+  }
+  
   render() {
-    return <></>
+    // console.log(this.state)
+    return (
+      <div className="content-container">
+        awef
+        <form>
+          <input type="file" onChange={this.handleFile}/>
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    )
   }
 }
+
+
+export default UploadForm
