79446b7fca88f2e2fe081403a63128db1328122f
appkenny <kzeng.95@gmail.com>
2021-03-16 03:08:55 -0700

    added clear track errors to upload props

--- frontend/components/upload_form/upload_form_container.js
+++ frontend/components/upload_form/upload_form_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import UploadForm from './upload_form'
-import { createTrack } from '../../actions/track_actions'
+import { createTrack, clearTrackErrors } from '../../actions/track_actions'
 
 const mapStateToProps = (state) => {
   return {
@@ -12,6 +12,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     createTrack: track => dispatch(createTrack(track)),
+    clearTrackErrors: () => dispatch(clearTrackErrors()),
   }
 }
 
