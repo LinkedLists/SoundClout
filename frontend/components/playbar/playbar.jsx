8bb6a2b8c75fbf601781b4e919710eb752a77841
appkenny <kzeng.95@gmail.com>
2021-03-20 22:38:52 -0700

    added a mute/unmute button and reduced play/pause to a single button on the play bar

--- frontend/components/playbar/playbar.jsx
+++ frontend/components/playbar/playbar.jsx
@@ -3,18 +3,59 @@ import React from 'react';
 class Playbar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state= {
+      playing: false,
+      muted: false
+    }
+
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handleMute = this.handleMute.bind(this);
+  }
+
+  // componentDidMount() {
+  //   let audio = document.getElementById('audio')
+  //   this.setState( {playing: audio.paused} )
+  // }
+
+  // componentDidUpdate() {
+  //   let audio = document.getElementById('audio')
+  //   this.setState( {playing: audio.paused} )
+  // }
+
+  handlePlay() {
+    let audio = document.getElementById('audio')
+    if (this.state.playing) {
+      audio.pause()
+      this.setState( {playing: false} )
+    } else {
+      audio.play()
+      this.setState( {playing: true} )
+    }
+  }
+
+  handleMute() {
+    let audio = document.getElementById('audio')
+    if (this.state.muted) {
+      audio.muted = false;
+      this.setState( {muted: false} )
+    } else {
+      audio.muted = true;
+      this.setState( {muted: true} )
+    }
   }
 
   render() {
     if (this.props.currentSessionId === null) return <></>
-
     return (
       <div className="playbar-footer">
         <div className="playbar-footer-wrapper">
           <div className="media-container">
             <audio id='audio' src={this.props.currentTrack.audioUrl} />
-            <button onClick={() => document.getElementById('audio').play()}>play</button>
-            <button onClick={() => document.getElementById('audio').pause()}>pause</button>
+            <button onClick={this.handlePlay}>{this.state.playing ? "Pause" : "Play"}</button>
+            {/* <button onClick={() => audio.pause()}>pause</button> */}
+            <button onClick={this.handleMute}>{this.state.muted ? "Unmute" : "Mute"}</button>
+            {/* <button onClick={() => audio.muted = false}>unmute</button> */}
           </div>
           <div className="current-track">
             { 
