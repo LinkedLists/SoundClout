132ca3963bfd86ddb7a51ec28e1633768b27ffc6
appkenny <kzeng.95@gmail.com>
2021-03-24 12:36:11 -0700

    removing comments slice of state because comment data is already in tracks

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,12 +2,11 @@ import { combineReducers } from 'redux';
 import userReducer from './users_reducer';
 import tracksReducer from './tracks_reducer'
 import CommentsReducer from './comments_reducer'
-import CommentForm from '../components/comment_form/comment_form';
 
 const entitiesReducer = combineReducers({
   users: userReducer,
   tracks: tracksReducer,
-  comments: CommentsReducer
+  // comments: CommentsReducer
 })
 
 export default entitiesReducer
