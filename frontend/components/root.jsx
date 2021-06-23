9bbd5adeac9c54383d05f3bdb0e1ac96015b0007
appkenny <kzeng.95@gmail.com>
2021-06-23 15:49:29 -0700

    add history-ga in root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./app";
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const Root = ({ store }) => (
-  <Provider store={store}>
+  <Provider store={store} history={history}>
     <HashRouter>
       <App />
     </HashRouter>
